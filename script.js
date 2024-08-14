f606a4315f519fe619279dc25954348443ca6f28
atat2 <71809445+atat2@users.noreply.github.com>
2024-08-14 17:03:39 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,39 @@
-window.onscroll = function() {
-    if(document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-        document.getElementById('backToTopBtn').style.display = 'block';
+$(window).on('scroll', function() {
+    if($(document).scrollTop() > 100) {
+        $("#backToTopBtn").css('display', 'block');
     } else {
-        document.getElementById('backToTopBtn').style.display = 'none';
+        $("#backToTopBtn").css('display', 'none');
     }
-};
+});
 
-document.getElementById('backToTopBtn').onclick = function() {
+$("#backToTopBtn").click(function() {
     window.scrollTo(0, 0)
-};
+});
+
+$(document).ready(function() {
+    const starCount = 100;
+    for ( let i = 0; i < starCount; i++) {
+        const star = $('<div class="star"></div>');
+        const posX = Math.random() * 100;
+        const posY = Math.random() * 100;
+        const delay = Math.random() * 5;
+
+        star.css({
+            top: posY + 'vh',
+            left: posX + 'vw',
+            animationDelay: delay + 's',
+            animationDuration: (2 + Math.random() *2) + 's'
+        });
+    
+        $('.stars').append(star);
+    }
+
+    $('#nightModeBtn').click(function(){
+        $('body').toggleClass('night-mode');
+        if($('body').hasClass('night-mode')) {
+            $('.stars').show();
+        } else {
+            $('.stars').hide();
+        }
+    });
+});
